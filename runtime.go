a6fa6d1ef3122496ad98b57cbc91668d664aabd0
Dmitry Panov <dop@itoolabs.com>
2021-10-06 13:25:47 +0100

    Support conversion from string types to []byte. Fixes #333
    Signed-off-by: Gabri <gabriele.cimato@gmail.com>
    (cherry picked from commit 6cc26dcbb13c26497b785212c33aa44240a29eda)

--- runtime.go
+++ runtime.go
@@ -38,6 +38,7 @@ var (
 	typeValue    = reflect.TypeOf((*Value)(nil)).Elem()
 	typeObject   = reflect.TypeOf((*Object)(nil))
 	typeTime     = reflect.TypeOf(time.Time{})
+	typeBytes    = reflect.TypeOf(([]byte)(nil))
 )
 
 type iterationKind int
@@ -1993,7 +1994,7 @@ func (r *Runtime) toReflectValue(v Value, dst reflect.Value, ctx *objectExportCt
 			return nil
 		}
 		expKind := et.Kind()
-		if expKind == kind && et.ConvertibleTo(typ) {
+		if expKind == kind && et.ConvertibleTo(typ) || expKind == reflect.String && typ == typeBytes {
 			ev := reflect.ValueOf(exportValue(v, ctx))
 			for ; i > 0; i-- {
 				ev = ev.Elem()
