8ceff1239103b322c6a00858df177cae2340ed5b
asutoshdhakal <asutosh.dhakal@mongodb.com>
2022-03-03 13:30:29 -0600

    REALMC-11774: support conversion to JS Uint8Array from a Go byte slice

--- runtime.go
+++ runtime.go
@@ -1784,6 +1784,14 @@ func (r *Runtime) ToValue(i interface{}) Value {
 		obj.self = a
 		a.init()
 		return obj
+	case []uint8:
+		buf := r._newArrayBuffer(r.global.ArrayBufferPrototype, nil)
+		buf.data = i
+		o := &Object{runtime: r}
+		a := r.newUint8ArrayObject(buf, 0, len(buf.data), r.global.Uint8Array)
+		o.self = a
+		a.init()
+		return o
 	}
 
 	origValue := reflect.ValueOf(i)
