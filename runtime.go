5f66509278bd5f11af9e4309d0019117fc94ee9f
Dmitry Panov <dop@itoolabs.com>
2022-05-16 13:39:00 +0100

    Do not use fmt.Sprintf() for plain error strings. Fixes #388.
    (cherry picked from commit 4418d4575a419bfb9a44250c8f2320f24b6c43b5)
    (cherry picked from commit 272ec8e4d380b5ed077e962a5439442d168a88e0)

--- runtime.go
+++ runtime.go
@@ -548,7 +548,12 @@ func (r *Runtime) MemUsage(ctx *MemUsageContext) (uint64, error) {
 }
 
 func (r *Runtime) newError(typ *Object, format string, args ...interface{}) Value {
-	msg := fmt.Sprintf(format, args...)
+	var msg string
+	if len(args) > 0 {
+		msg = fmt.Sprintf(format, args...)
+	} else {
+		msg = format
+	}
 	return r.builtin_new(typ, []Value{newStringValue(msg)})
 }
 
