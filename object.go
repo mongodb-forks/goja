370aaa37d2dad4959384dc551b5e194419698b77
Dmitry Panov <dop@itoolabs.com>
2022-07-29 11:23:23 +0100

    Added NewSharedDynamicObject() and NewSharedDynamicArray(). Closes #418.
    (cherry picked from commit fca9b959775c67aeaa16687f1ddafdce2d7fef83)
    (cherry picked from commit 7fa756831279dabb77eff3eaabb7c3994f585f69)

--- object.go
+++ object.go
@@ -1504,7 +1504,7 @@ func toMethod(v Value) func(FunctionCall) Value {
 			return call
 		}
 	}
-	panic(typeError(fmt.Sprintf("%s is not a method", v.String())))
+	panic(newTypeError("%s is not a method", v.String()))
 }
 
 func instanceOfOperator(o Value, c *Object) bool {
