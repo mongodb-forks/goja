759f845115ee6fb8112812235fd0e4567b7221ea
Gabriele Cimato <gabriele.cimato@gmail.com>
2023-08-03 13:28:37 -0500

    feat: return dual value for mem usage in object_lazy

---
+++ object_lazy_test.go
@@ -0,0 +1,55 @@
+package goja
+
+import (
+	"testing"
+)
+
+func TestObjectLazyMemUsage(t *testing.T) {
+	tests := []struct {
+		name        string
+		val         *lazyObject
+		expected    uint64
+		newExpected uint64
+		errExpected error
+	}{
+		{
+			name:        "should have a value of SizeEmpty given a nil lazy object",
+			val:         nil,
+			expected:    SizeEmpty,
+			newExpected: SizeEmpty,
+			errExpected: nil,
+		},
+		{
+			name:        "should have a value of SizeEmpty given an empty lazy object",
+			val:         &lazyObject{},
+			expected:    SizeEmpty,
+			newExpected: SizeEmpty,
+			errExpected: nil,
+		},
+		{
+			name:        "should have a value of SizeEmpty given a base dynamic array with an empty val",
+			val:         &lazyObject{val: &Object{}},
+			expected:    SizeEmpty + SizeEmpty,
+			newExpected: SizeEmpty + SizeEmpty,
+			errExpected: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			total, newTotal, err := tc.val.MemUsage(NewMemUsageContext(New(), 100, 100, 100, 100, nil))
+			if err != tc.errExpected {
+				t.Fatalf("Unexpected error. Actual: %v Expected: %v", err, tc.errExpected)
+			}
+			if err != nil && tc.errExpected != nil && err.Error() != tc.errExpected.Error() {
+				t.Fatalf("Errors do not match. Actual: %v Expected: %v", err, tc.errExpected)
+			}
+			if total != tc.expected {
+				t.Fatalf("Unexpected memory return. Actual: %v Expected: %v", total, tc.expected)
+			}
+			if newTotal != tc.newExpected {
+				t.Fatalf("Unexpected new memory return. Actual: %v Expected: %v", newTotal, tc.newExpected)
+			}
+		})
+	}
+}
